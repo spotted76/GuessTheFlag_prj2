a94a77f008a7ec54684c7e75dfc75815b82d00bf
Peter Fischer <spotted@gmail.com>
2022-03-04 19:15:21 -0500

    Initial Commit

---
+++ GuessTheFlag_prj2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GuessTheFlag_prj2
+//
+//  Created by Peter Fischer on 3/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GuessTheFlag_prj2/GuessTheFlag_prj2App.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlag_prj2App.swift
+//  GuessTheFlag_prj2
+//
+//  Created by Peter Fischer on 3/4/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlag_prj2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
