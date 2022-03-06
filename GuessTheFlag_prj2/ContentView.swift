e555eacfbec9c799d54ad9e361e80c44edd7cc90
Peter Fischer <spotted@gmail.com>
2022-03-05 19:45:10 -0500

    Changes before messing with design

--- GuessTheFlag_prj2/ContentView.swift
+++ GuessTheFlag_prj2/ContentView.swift
@@ -8,14 +8,75 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            LinearGradient(colors: [Color.blue, Color.black], startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+            VStack(spacing: 30) {
+
+                VStack {
+                    Text("Tap The Flag Of:")
+                        .foregroundColor(.white)
+                        .font(.subheadline.weight(.heavy))
+                    
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                        .font(.largeTitle.weight(.semibold))
+                }
+            
+                ForEach(0..<3) { number in
+                    Button {
+                        flagTapped(number)
+                    } label: {
+                        Image(countries[number])
+                            .renderingMode(.original)
+                            .clipShape((Capsule()))
+                            .shadow(radius: 5)
+                    }
+                }
+        
+            }
+    
+        }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue") {
+                askQuestion()
+            }
+        } message: {
+            Text("Your Score is ???")
+        }
+        
+    }
+    
+    func flagTapped(_ number : Int) {
+        if ( number == correctAnswer) {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Incorrect"
+        }
+        
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        VStack {
+            ContentView()
+        }
+.previewInterfaceOrientation(.portrait)
     }
 }
